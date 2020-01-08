8416fbd78549b0899eb54ffe8026043a92540890
Remco van 't Veer <remco@remworks.net>
2020-01-08 08:57:55 +0100

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject markov-chain "0.1.0-SNAPSHOT"
+(defproject markov-chain "0.1.0"
   :description "A Clojure library to generate data using Markov chain probabilities."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/remvee/markov"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.generators "0.1.2"]]
   :repl-options {:init-ns remworks.markov-chain})

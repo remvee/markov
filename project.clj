e1e86b70b40253545e6b18c05f89392e1c318c67
Remco van 't Veer <remco@remworks.net>
2022-09-05 10:43:27 +0200

    Fix project link

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject markov-chain "0.2.0"
   :description "A Clojure library to generate data using Markov chain probabilities."
-  :url "https://github.com/remvee/markov"
+  :url "https://git.sr.ht/~rwv/markov"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
